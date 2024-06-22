9020739dc2ef4dc31c3404e107600520eaab6296
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 18:04:47 +0300

    Добавьте файлы проекта.

---
+++ LabWork31/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LabWork31
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            datePicker.DisplayDateStart = new DateTime(1900, 1, 1);
+            datePicker.DisplayDateEnd = DateTime.Now;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(LoginTextBox.Text) && !String.IsNullOrEmpty(PasswordTextBox.Password) &&
+                 PasswordTextBox.Password == ConfirmPasswordBox.Password)
+            {
+                MessageBox.Show(LoginTextBox.Text + ", вы зарегистрированы.");
+            }
+            else 
+            {
+                MessageBox.Show("Введены не все данные или пароли не совпадают.");
+            }
+
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime birthDate = datePicker.SelectedDate ?? DateTime.Now;
+
+            TimeSpan age = DateTime.Now - birthDate;
+            int years = (int)(age.Days / 365.25);
+
+            ageLabel.Text = $"Age: {years} years";
+        }
+    }
+}
---
+++ task4/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PriceSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            summa.Content =  $"Сумма: {Math.Round(PriceSlider.Value,2)}";
+            result.Content = $"Результат: {Math.Round((PriceSlider.Value*ProcentSlider.Value/100),2)}";
+        }
+
+        private void ProcentSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            percent.Content = $"Процент: {Math.Round(ProcentSlider.Value,2)}";
+            result.Content = $"Результат: {Math.Round((PriceSlider.Value * ProcentSlider.Value / 100), 2)}";
+        }
+    }
+}
